4a2192aee821a431ab893b471849ea82b87181f9
RASHEED AL-QADHI (ȘħλɖΘώ) <62840251+shadowYEM@users.noreply.github.com>
2023-06-10 03:15:58 +0300

    Update weathers.py

--- weathers.py
+++ weathers.py
@@ -3,6 +3,7 @@ import pandas as pd
 import requests
 import plotly.express as px
 
+
 api_key = "d189e6e5c1734d2e803221317230706"
 base_url = "http://api.weatherapi.com/v1/"
 
@@ -14,6 +15,7 @@ def get_weather(city: str):
     temperature = data["current"]["temp_c"]
     description = data["current"]["condition"]["text"]
     return temperature, description
+
 
 def get_forecast(city: str, days: int):
     complete_url = f"{base_url}forecast.json?key={api_key}&q={city}&days={days}&hourly=1"
@@ -27,6 +29,7 @@ def get_forecast(city: str, days: int):
             dates.append(hour["time"])
             temperatures.append(hour["temp_c"])
     return dates, temperatures
+
 
 arab_countries = {
      "Yemen": ["Sana'a","'Adan","'Amran","Al Bayda'","Al Hudaydah","Al Jawf","Al Mahrah","Al Mahwit","Amanat al 'Asimah","Dhamar","Hadhramaut","Hajjah","Ibb","Lahij","Ma'rib","Raymah","Sa'dah","Sana'a (governorate)","Shabwah","Socotra Island","Ta'izz"],
@@ -46,6 +49,7 @@ dates, temperatures = get_forecast(province1, 1)
 fig = px.line(x=dates, y=temperatures, labels={"x": "Time", "y": "Temperature (°C)"})
 st.plotly_chart(fig)
 
+
 st.title("Weather Forecast")
 country2 = st.selectbox("Select a country:", list(arab_countries.keys()), key="country2")
 province2 = st.selectbox("Select a province:", arab_countries[country2], key="province2")
@@ -56,16 +60,15 @@ dates, temperatures = get_forecast(province2, days)
 fig = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (°C)"})
 st.plotly_chart(fig)
 
+# data = {"Date": dates, "Temperature (°C)": temperatures}
+# df = pd.DataFrame(data)
+
+# col1, col2, col3 = st.columns([1, 2, 1])
+# with col2:
+#     st.table(df)
 data = {"Date": dates, "Temperature (°C)": temperatures}
 df = pd.DataFrame(data)
 
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.table(df)
-
-# Example usage of the get_forecast function
-# city = "Riyadh"
-# dates, temperatures = get_forecast(city, 1)
-# plot_temperatures(dates, temperatures)
-
-
+    st.dataframe(df)
